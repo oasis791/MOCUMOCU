a1d98581cefc3eb8fafebafedb74c0c65ab18ff5
oasis791 <khwi791@naver.com>
2022-05-04 22:02:35 +0900

    Initial Commit

---
+++ back/project/src/test/java/MOCUMOCU/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package MOCUMOCU.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
