9a10df0f64d35f3601768d1765f3890ca1da7271
lee2963 <sodel2963@naver.com>
2022-05-06 00:11:14 +0900

    Entity update

---
+++ back/project/src/main/java/MOCUMOCU/project/domain/Reward.java
@@ -0,0 +1,22 @@
+package MOCUMOCU.project.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+public class Reward {
+
+    @Id @GeneratedValue
+    @Column(name = "reward_id")
+    private Long id;
+
+    private int needAmount;
+    private String rewardContent;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name= "market_id")
+    private Market market;
+}
