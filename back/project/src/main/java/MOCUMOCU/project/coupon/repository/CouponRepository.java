01176669f133df1b5bd0d6a336b0ae7d630e2155
oasis791 <khwi791@naver.com>
2024-03-12 22:33:58 +0900

    feat: JPARepository extends한 인터페이스로 저장소 변경

---
+++ back/project/src/main/java/MOCUMOCU/project/coupon/repository/CouponRepository.java
@@ -0,0 +1,19 @@
+package MOCUMOCU.project.coupon.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import MOCUMOCU.project.coupon.entity.Coupon;
+
+public interface CouponRepository extends JpaRepository<Coupon, Long> {
+
+	@Query(value = "SELECT c FROM Coupon c WHERE c.customer.id = :customerId AND c.market.id = :marketId")
+	Optional<Coupon> findByCustomerIdAndMarketId(@Param("customerId") Long customerId, @Param("marketId") Long marketId);
+
+	@Query(value = "SELECT c FROM Coupon c WHERE c.customer.id = :customerId")
+	Optional<List<Coupon>> findByCustomerId(@Param("customerId") Long customerId);
+}
